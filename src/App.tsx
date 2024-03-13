7dd5699aa60103c00a530fa96c015a03f5efd8bc
Alex <sheeetface333@gmail.com>
2024-03-13 17:08:13 +0300

    atomic design

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import './styles/common.scss'
 import './app.scss'
+
 
 function App() {
 
   return (
-    <div className="App">Poppins - Google Font</div>
+    <div className="App">
+
+      <div className='header'>
+        <div className='center'>
+          <div className='container'>
+            <div className='left'>
+              <div className='map-pin-icon'>1</div>
+              <span>Poppins</span>
+              </div>
+            <div className='right'>Google Font</div>
+          </div>
+        </div>
+      </div>
+
+      </div>
   )
 }
 
 export default App
+
