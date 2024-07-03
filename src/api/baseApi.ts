b0be5ee46a0a5ad358b32e1a57611e9abc49ea15
Alex <sheeetface333@gmail.com>
2024-07-03 17:57:36 +0300

    Implemented fallback mechanism to switch to secondary server in RTK Query for baseApi

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -1,9 +1,45 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react";
+
+import type { BaseQueryFn, FetchArgs, FetchBaseQueryError } from "@reduxjs/toolkit/query/react";
+
+type BaseQueryWithFallback = BaseQueryFn<string|FetchArgs, unknown, FetchBaseQueryError>;
+
 
 const baseUrl = import.meta.env.VITE_APP_BASE_API_URL;
+const baseUrlReserve = import.meta.env.VITE_APP_BASE_API_URL_RESERVE;
+
+const switchToBackupAlertLog = '%c The primary server is not responding. Switching to the backup.';
+const backupNotResponseAlertLog = '%c The backup server is also not responding.';
+
+const switchToBackupAlertLogStyle = 'color: orange; font-size: 16px; font-weight: bold;';
+const backupNotResponseAlertLogStyle = 'color: tomato; font-size: 18px; font-weight: bold;';
+
+const baseQuery = fetchBaseQuery({ baseUrl });
+const baseQueryReserve = fetchBaseQuery({ baseUrl: baseUrlReserve });
+
+const baseQueryWithFallback: BaseQueryWithFallback = async(args, api, extraOptions)=>{
+  let result;
+
+  try{
+    result = await baseQuery(args, api, extraOptions);
+
+    if(result.error && result.error.status === 'FETCH_ERROR'){
+      console.log(switchToBackupAlertLog,switchToBackupAlertLogStyle)
+      result = await baseQueryReserve(args, api, extraOptions);
+    }
+  }catch(error){
+    console.log(switchToBackupAlertLog,switchToBackupAlertLogStyle)
+    result = await baseQueryReserve(args, api, extraOptions);
+  }
+
+  if(result.error) console.log(backupNotResponseAlertLog,backupNotResponseAlertLogStyle);
+  
+  return result;
+}
+
+const baseQueryWithRetry = retry(baseQueryWithFallback,{ maxRetries:1});
 
 export const baseApi = createApi({
-    baseQuery: fetchBaseQuery({baseUrl}),
-    endpoints: () =>({})
-})
-
+  baseQuery: baseQueryWithRetry,
+  endpoints: () => ({})
+});
--- src/hooks/useApiResource.tsx
+++ src/hooks/useApiResource.tsx
@@ -1,69 +1,75 @@
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import FullScreenLoader from "../components/organisms/FullScreenLoader/FullScreenLoader";
 import AlertMessage from "../components/molecules/AlertMessage/AlertMessage";
-import NotingFound from "../components/atoms/NothingFound/NothingFound";
+import NothingFound from "../components/atoms/NothingFound/NothingFound";
 
 import { shopProductData } from "../data/temp/shopProductData";
 
-import type { UseQuery,BaseQueryFn, FetchBaseQueryError, FetchBaseQueryMeta } from '@reduxjs/toolkit/query/react';
-import type { QueryDefinition,FetchArgs } from "@reduxjs/toolkit/query";
+import type { UseQuery, BaseQueryFn, FetchBaseQueryError, FetchBaseQueryMeta } from '@reduxjs/toolkit/query/react';
+import type { QueryDefinition, FetchArgs } from "@reduxjs/toolkit/query";
 
-
-interface UseApiResourceReturn<R> {
-    responseData: R
-    content: JSX.Element | null
+interface UseApiResourceReturn<R>{
+  responseData: R|null;
+  content: JSX.Element|null;
 }
 
-type QueryHook<R> = () => UseQuery<
-    QueryDefinition<
-        void,
-        BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError, Record<string, unknown>, FetchBaseQueryMeta>,
-        never,
-        R,
-        "api"
-    >
+type QueryHook<R> =()=>UseQuery<
+  QueryDefinition<
+    void,
+    BaseQueryFn<string|FetchArgs, unknown, FetchBaseQueryError, Record<string, unknown>, FetchBaseQueryMeta>,
+    never,
+    R,
+    "api"
+  >
 >
 
-type TypeResource = 'products'|'blogs'
+type TypeResource = 'products'|'blogs';
 
+const useApiResource = <R,>(query: QueryHook<R>, typeResource: TypeResource): UseApiResourceReturn<R> => {
 
-const useApiResource = <R,> (query: QueryHook<R>,typeResource:TypeResource): UseApiResourceReturn<R> => {
-    const { data, isLoading, isError,error } = query();
-    
-    let content = null;
+    const { data, isLoading, isError, error } = query();
 
-    if (isLoading ) content = <FullScreenLoader />;
-    if (!isLoading && !isError && Array.isArray(data) && data.length === 0) content = <NotingFound />;
-    if (isError) content = <AlertMessage type='error'
-                                         title='FETCH ERROR'
-                                         isCanClose={true}
-                                         message={
-                                            <>
-                                                <b>
-                                                 Sorry, but the data server is temporarily unavailable😤 This may be due to the fact that it is free. In such a case, we refer to the locally hosted data.
-                                                </b>
-                                                <br/>
-                                                {error.data.message}
-                                            </>
-                                         }    
-                            /> 
+    const [responseData, setResponseData] = useState<R|null>(null);
 
-    const isResponseError =useCallback((type:TypeResource)=>{
-
+    const isResponseError = useCallback((type: TypeResource)=>{
         switch(type){
-            case "products":
-                return shopProductData
-            // case "blogs":
-            //     return
+        case "products":
+            return shopProductData;
+        // case "blogs":
+        //   return
+        default:
+            return null;
         }
+    },[typeResource]);
 
-    },[query,typeResource])
+    useEffect(()=>{
+        if(data) setResponseData(data);
+        else if(isError) setResponseData(isResponseError(typeResource) as R|null);
+    },[data, isError, typeResource, isResponseError]);
 
-    return  { 
-                responseData:isError ? isResponseError(typeResource):data,
-                content
-            }
-};
+    let content: JSX.Element|null = null;
+
+    if(isLoading) content = <FullScreenLoader />;
+    else if(!isLoading && !isError && Array.isArray(data) && data.length === 0) content = <NothingFound />;
+    else if(isError) content = (
+                        <AlertMessage
+                            type='error'
+                            title='FETCH ERROR'
+                            isCanClose={true}
+                            message={
+                            <>
+                                <b>
+                                    Sorry, but the servers (both ZEABUR and APPWRITE) are temporarily unavailable😤 This may be because it's free. In that case we are accessing locally hosted data.
+                                </b>
+                                <br />
+                                {(error?.data?.message) ?? (error?.error) ?? 'Unknown error'}
+                            </>
+                            }
+                        />
+                    );
+
+    return {responseData,content};
+}
 
 export default useApiResource;
