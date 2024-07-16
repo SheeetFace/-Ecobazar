c491d8f292124c280a884fad8a8f21179cecbaec
Alex <sheeetface333@gmail.com>
2024-07-16 15:51:52 +0300

    FIX:SaleProductCard styles and get data

--- src/components/organisms/pages/shopPage/SaleProducts/SaleProducts.tsx
+++ src/components/organisms/pages/shopPage/SaleProducts/SaleProducts.tsx
@@ -1,33 +1,38 @@
+import useApiResource from '../../../../../hooks/useApiResource';
+
+import { useGetProductsQuery } from '../../../../../api/products/productApi';
 
 import SaleProductCard from '../../../../molecules/card/SaleProductCard/SaleProductCard';
 
-import { saleProductsShopData } from '../../../../../data/temp/saleProductsShopData';
-
 import styles from '../SaleProducts/SaleProducts.module.scss';
+
+import type { ProductDataType } from '../../../../../types/productDataTypes';
+import { useMemo } from 'react';
 
 const SaleProducts:React.FC = () => {
 
-    const renderSaleProductsCard =()=>{
-        return saleProductsShopData.map((item,i)=>{
-            return(
-                <SaleProductCard    name={item.name}
-                                    id={item.id}
-                                    src={item.src}
-                                    currentCost={item.currentCost}
-                                    oldCost={item.oldCost}
-                                    rating={item.rating}
-                                    key={i}
-                />
-            )
-        })
-    }
+    const {responseData, content} = useApiResource<ProductDataType>(useGetProductsQuery, 'products');
+
+    const renderSaleProductsCard = useMemo(()=>{
+        if(responseData){
+            const saleProductsShopData = [responseData[2],responseData[9],responseData[12]];
+
+            return saleProductsShopData.map((item)=>{
+                return(
+                    <SaleProductCard key={item.id} {...item}/>
+                )
+            })
+        }
+    },[responseData])
 
     return (
         <section className={styles.SaleProducts}>
             <h2>Sale Products</h2>
 
             <div className={styles._container}>
-                {renderSaleProductsCard()}
+                {content}
+                {responseData ? renderSaleProductsCard :null}
+                {/* {renderSaleProductsCard()} */}
             </div>
 
         </section>
