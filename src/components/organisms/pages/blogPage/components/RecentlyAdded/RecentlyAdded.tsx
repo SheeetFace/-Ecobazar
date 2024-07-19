134e40b11cd8f3a468bd5e75edb076c72797a36b
Alex <sheeetface333@gmail.com>
2024-07-19 09:21:17 +0300

    FIX: RecentlyAdded for blog gets data from api. Change a few styles in RecentlyAddedBlogCard

--- src/components/organisms/pages/blogPage/components/RecentlyAdded/RecentlyAdded.tsx
+++ src/components/organisms/pages/blogPage/components/RecentlyAdded/RecentlyAdded.tsx
@@ -1,31 +1,37 @@
+import { useMemo } from 'react';
+
+import useApiResource from '../../../../../../hooks/useApiResource';
+import { useGetLatestBlogsQuery } from '../../../../../../api/blogs/latestBlogsApi';
 
 import RecentlyAddedBlogCard from '../../../../../molecules/card/RecentlyAddedBlogCard/RecentlyAddedBlogCard';
 
-import { blogsData } from '../../../../../../data/temp/blogsData';
-
 import styles from '../RecentlyAdded/RecentlyAdded.module.scss';
+
+import type { BlogDataTypes } from '../../../../../../types/blogDataTypes';
+
 
 const RecentlyAdded:React.FC = () => {
 
-    const recentlyBlogs = blogsData.slice((blogsData.length-3),blogsData.length)
+    const {responseData, content} = useApiResource<BlogDataTypes>(useGetLatestBlogsQuery, 'latestBlogs');
 
-    const renderRecentlyBlogs=()=>{
-        return recentlyBlogs.reverse().map((item,i)=>{
-            return <RecentlyAddedBlogCard  
-                                        title={item.title}
-                                        src={item.src}
-                                        date={item.date}
-                                        id={item.id}
-                                        key={i}
-                    />
-        })
-    }
-
+    const renderRecentlyBlogs=useMemo(()=>{
+        if(responseData){
+            return responseData.map((item)=>{
+                return <RecentlyAddedBlogCard   key={item.id}
+                                                title={item.title}
+                                                src={item.src}
+                                                date={item.date}
+                                                id={item.id}
+                        />
+            })
+        }
+    },[responseData])
 
     return (
         <div className={styles.RecentlyAdded}>
             <h1>Recently Added</h1>
-            {renderRecentlyBlogs()}
+            {content}
+            {renderRecentlyBlogs}
         </div>
     )
 }
