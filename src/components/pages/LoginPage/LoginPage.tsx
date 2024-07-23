44ed7409d0ac4e60a3b655a3e3a5b3faca435838
Alex <sheeetface333@gmail.com>
2024-07-23 11:23:58 +0300

    FEAT:UseScrollToTop hook now accepts the top argument (in px). UseScrollToTop added in LoginPage

--- src/components/pages/LoginPage/LoginPage.tsx
+++ src/components/pages/LoginPage/LoginPage.tsx
@@ -1,8 +1,12 @@
+import useScrollToTop from '../../../hooks/useScrollToTop';
+
 import Login from '../../organisms/pages/loginAndRegistrationPage/Login/Login';
 
 import styles from '../LoginPage/LoginPage.module.scss';
 
 const LoginPage:React.FC = () => {
+
+    useScrollToTop(0,'smooth',200);
 
     return (
         <section className={styles.LoginPage}>
