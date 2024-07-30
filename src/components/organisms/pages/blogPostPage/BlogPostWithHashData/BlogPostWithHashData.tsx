d8fe728595ee71d428cd10989e42fa5a27cab97a
Alex <sheeetface333@gmail.com>
2024-07-30 12:32:23 +0300

    REFACTOR: created useFetchDataByHash hook for ProductPageWithHashData and BlogPostWithHashData fetching and redirection if using hash

--- src/components/organisms/pages/blogPostPage/BlogPostWithHashData/BlogPostWithHashData.tsx
+++ src/components/organisms/pages/blogPostPage/BlogPostWithHashData/BlogPostWithHashData.tsx
@@ -1,22 +1,15 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
 import { useGetBlogsQuery } from "../../../../../api/blogs/blogsApi";
+import useFetchDataByHash from "../../../../../hooks/useFetchDataByHash";
 
 import BlogPostContent from "../BlogPostContent/BlogPostContent";
 import FullScreenLoader from "../../../FullScreenLoader/FullScreenLoader";
 
+import { BlogDataTypes } from "../../../../../types/blog/blogDataTypes";
+
 
 const BlogPostWithHashData:React.FC<{ hash: string }> = ({ hash }) => {
 
-    const navigate = useNavigate();
-    const { data, isLoading } = useGetBlogsQuery();
-
-    const hashData = data?.list?.find((blog)=>blog.id === hash);
-
-    useEffect(() => {
-        if(!isLoading && !hashData) navigate('/blog');
-    },[hashData,isLoading]);
+    const {hashData, isLoading} = useFetchDataByHash<BlogDataTypes>(hash,useGetBlogsQuery,'/blog')
 
     if(isLoading) return <FullScreenLoader/>;
     if(hashData) return <BlogPostContent data={hashData}/>;
---
+++ src/hooks/useFetchDataByHash.ts
@@ -0,0 +1,67 @@
+import { useEffect, useMemo } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import type { QueryHook } from "../types/queryHookTypes";
+
+// type Query<T> = QueryHook<{ map?: Map<string, T>, list?: T[] }>
+
+
+type QueryData<T> = { map?: Map<string, T>, list?: T[] };
+
+type Query<T> = QueryHook<QueryData<T>>;
+
+
+const isMap = <T,>(data:QueryData<T>): data is { map:Map<string,T> } => {
+  return data.map !== undefined && data.map instanceof Map;
+}
+const isList = <T,>(data:QueryData<T>): data is { list:T[] } => {
+  return data.list !== undefined && Array.isArray(data.list);
+}
+
+
+const useFetchDataByHash = <T extends { id:string}> (hash:string, query:Query<T>, redirect:string) => {
+  const navigate = useNavigate();
+  const { data, isLoading } = query();
+
+  const hashData = useMemo(():T|null=>{
+    if(data){
+      if(isMap(data)) return data.map.get(hash) as T;
+      if(isList(data)) return data.list.find((item) => (item as T).id === hash) as T;
+    }
+    return null;
+  },[data,hash])
+
+  useEffect(()=>{
+    if(!isLoading && !hashData) navigate(redirect);
+  },[hashData, isLoading, navigate, redirect])
+
+  return {hashData, isLoading};
+
+}
+
+export default useFetchDataByHash;
+
+
+// const useFetchDataByHash = <T,>(hash:string,query:Query<T>,redirect:string)=>{
+    
+//     const navigate = useNavigate();
+
+//     const { data, isLoading} = query();
+
+//     const hashData = useMemo((data,hash)=>{
+
+//         if(data){
+//             if(data instanceof Map) return data?.map?.get(hash);
+//             if(data instanceof Array) return data?.list?.find((blog)=>blog.id === hash);
+//         }
+
+//     },[data])
+
+
+//     useEffect(() => {
+//         if(!isLoading && !hashData) navigate(redirect);
+//     },[hashData,isLoading]);
+
+// }
+// export default useFetchDataByHash;
