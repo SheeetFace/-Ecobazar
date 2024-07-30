379bcaa8d732f96698fe10a06a9cf88e4a137186
Alex <sheeetface333@gmail.com>
2024-07-30 15:37:02 +0300

    REFACTOR: improved OrderDetailDashboardPage structure. FEAT: added support for fetching HistoryOrders data via URL hash

--- src/components/organisms/pages/checkoutPage/OrderSummery/components/SubmitOrderForm/SubmitOrderForm.tsx
+++ src/components/organisms/pages/checkoutPage/OrderSummery/components/SubmitOrderForm/SubmitOrderForm.tsx
@@ -56,7 +56,7 @@ const SubmitOrderForm:React.FC = () => {
                         dispatch(setShippingInfo([]))
                     ]);
                     
-                    navigate('/dashboard/order-detail',{state: {res}});
+                    navigate(`/dashboard/order-detail#${res.id}`,{state: {data:res}});
                 }
             }
         }
--- src/components/pages/OrderDetailDashboardPage/OrderDetailDashboardPage.tsx
+++ src/components/pages/OrderDetailDashboardPage/OrderDetailDashboardPage.tsx
@@ -1,60 +1,18 @@
-import { useLocation,NavLink } from 'react-router-dom';
+import useConditionalNavigation from '../../../hooks/useConditionalNavigation';
 import useScrollToTop from '../../../hooks/useScrollToTop';
 
-import { formatDate } from '../../../utils/formatDate';
-
-import Divider from '../../atoms/Divider/Divider';
-import BillingAndShipping from '../../molecules/pages/orderDetailDashboardPage/BillingAndShipping/BillingAndShipping';
-import SummaryPaid from '../../molecules/pages/orderDetailDashboardPage/SummaryPaid/SummaryPaid';
-import ProgressTracker from '../../molecules/pages/orderDetailDashboardPage/ProgressTracker/ProgressTracker';
-import PaidProductsTable from '../../organisms/pages/orderDetailDashboardPage/PaidProductsTable/PaidProductsTable';
-
-import styles from '../OrderDetailDashboardPage/OrderDetailDashboardPage.module.scss';
-
-import type { ILocation } from '../../../types/locationTypes';
-import { ResponseOrderDataType } from '../../../types/db/order/responseOrderDataType';
-
-type LocationResponseOrderDataType = {res:ResponseOrderDataType}
+import OrderDetailDashboardPageContent from '../../organisms/pages/orderDetailDashboardPage/OrderDetailDashboardPageContent/OrderDetailDashboardPageContent';
+import OrderDetailDashboardPageWithHashData from '../../organisms/pages/orderDetailDashboardPage/OrderDetailDashboardPageWithHashData/OrderDetailDashboardPageWithHashData';
 
 const OrderDetailDashboardPage:React.FC = () => {
 
     useScrollToTop(0,'smooth');
-    const data:ILocation<LocationResponseOrderDataType> = useLocation();
+    
+    const {data,hash} = useConditionalNavigation('/dashboard')
 
-    const {date,productIDs,id,status,totalPrice,shipping,paymentMethod} = data.state.res;
-
-    return (
-        <section className={styles.OrderDetailDashboardPage}>
-            <div className={styles._headerContainer}>
-                <div className={styles._info}>
-                    <h1>Order Detail</h1>
-                    <span>&#8226;</span>
-                    <span>{formatDate(date, 'full')}</span>
-                    <span>&#8226;</span>
-                    <span>{productIDs.length} Products</span>
-                </div>
-                <NavLink to='/dashboard/order-history' className='_navLinkGreenPrimaryLetterSpacing'>Back To List</NavLink>
-            </div>
-
-            <Divider type='horizontal' className={styles._divider}/>
-            
-            <div className={styles._container}>
-                <div className={styles._billingAndShipping}>
-                    <BillingAndShipping shipping={shipping}/>
-                </div>
-                <div className={styles._summerPaid}>
-                    <SummaryPaid id={id} totalPrice={totalPrice} paymentMethod={paymentMethod}/>
-                </div>
-            </div>
-            <div className={styles._tracker}>
-                <ProgressTracker status={status}/>
-            </div>
-
-            <div className={styles._table}>
-                <PaidProductsTable productIDs={productIDs}/>
-            </div>
-        </section>
-    )
+    if(data) return <OrderDetailDashboardPageContent data={data}/>
+    else if(!data && hash) return <OrderDetailDashboardPageWithHashData hash={hash}/>
+    else null
 }
 
 export default OrderDetailDashboardPage;
