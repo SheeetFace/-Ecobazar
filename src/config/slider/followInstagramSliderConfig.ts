c0a189f671bd22c71003819fa24efe5bab8ba4ef
Alex <sheeetface333@gmail.com>
2024-08-20 12:16:47 +0300

    STYLE: Added correct config for FollowUsOnInstagram's slider

---
+++ src/config/slider/followInstagramSliderConfig.ts
@@ -0,0 +1,31 @@
+
+export const followInstagramSliderConfig = [
+    {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        }
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 730,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 450,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+]
