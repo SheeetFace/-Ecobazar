2aa33dc00a8088e311859be4077390774608c896
Alex <sheeetface333@gmail.com>
2024-08-20 11:17:19 +0300

    STYLE: Added correct config for ClietnTestimonial's slider

--- src/components/organisms/ClientTestimonials/ClientTestimonials.tsx
+++ src/components/organisms/ClientTestimonials/ClientTestimonials.tsx
@@ -14,37 +14,44 @@ interface ClientTestimonialsData {
     rating:string
 }
 
+import { clientTestimonialsSliderConfig } from '../../../config/slider/clientTestimonialsSliderConfig';
+
 const ClientTestimonials:React.FC = () => {
 
     const renderTestimonials = ()=>{
         return clientTestimonialsData.map((item:ClientTestimonialsData,i)=>{
             return(
                     <div className={styles._cardItem} key={i}>
-                    <TestimonialCard    
-                                        id={item.id}
-                                        name={item.name}
-                                        avatar={item.avatar}
-                                        testimonial={item.testimonial}
-                                        rating={item.rating}
-                    />
+                        <TestimonialCard    
+                                            id={item.id}
+                                            name={item.name}
+                                            avatar={item.avatar}
+                                            testimonial={item.testimonial}
+                                            rating={item.rating}
+                        />
                     </div>
             )
         })
     }
 
-    const useSlicer = useSlider({ cards: renderTestimonials(),
-                                  styles: styles._settingSlider,
-                                  slidesToShow: 3,
-    });
+    const slider = useSlider({
+        cards:renderTestimonials(),
+        styles:styles._settingSlider,
+        slidesToShow:3,
+        dots:true,
+        responsiveSetting:clientTestimonialsSliderConfig,
+        isSliderShow:true,
+        cardsWithoutSliderStyles:styles._cards
+    })
 
     return (
         <section className={styles.ClientTestimonials}>
             <div className='center'>
-                <div>
+                <div className={styles._titleContainer}>
                     <span className={styles._title}>Client Testimonials</span>
                 </div>
 
-                {useSlicer}
+                {slider}
 
             </div>
         </section>
---
+++ src/config/slider/clientTestimonialsSliderConfig.ts
@@ -0,0 +1,17 @@
+
+export const clientTestimonialsSliderConfig =[
+      {
+        breakpoint: 950,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 610,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+]
